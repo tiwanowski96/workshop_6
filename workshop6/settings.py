0b7986ec7adea8db25af644f0742351acd2ad72e
Tomasz Iwanowski <t.iwanowski96@gmail.com>
2017-07-10 15:17:57 +0200

    settings

--- workshop6/settings.py
+++ workshop6/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'server',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'workshop6.wsgi.application'
 
 DATABASES = {
     'default': {
+        'NAME': 'base.db',
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'OPTIONS': {
+        },
     }
 }
 
