6224a2659459f580f5d6bb3f6b4b6e80251c2473
natancamenzind <kamilmarekradomski@gmail.com>
2017-07-10 15:38:27 +0200

    models + first migration

--- server/models.py
+++ server/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=97)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=97)
+    description = models.TextField()
+    director = models.ForeignKey(Person, related_name="directed_movies")
+    actors = models.ManyToManyField(Person, through="Play")
+    year = models.IntegerField()
+
+
+class Play(models.Model):
+    actor = models.ForeignKey(Person)
+    film = models.ForeignKey(Movie)
+    role = models.CharField(max_length=97)
+
